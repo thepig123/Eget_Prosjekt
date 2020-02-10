a1b2237dbb19e7c87346946d03d07ec2e591700f
thepig123 <martinemilolsen@gmail.com>
2020-02-11 00:40:28 +0200

    dog

---
+++ server.js
@@ -0,0 +1,66 @@
+if (process.env.NODE_ENV !== 'pruction') {
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email)
+)
+
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { name: 'Tore'})
+})
+
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+app.post('/register', async (req, res)  => {
+    try {
+        const hashedPasswords = await bcrypt.hash(req.body.password, 10) //Encrypts password
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPasswords
+        })
+        res.redirect('/Login')
+    } catch {
+        console.log('failed')
+    }
+    console.log(users)
+ })
+
+app.listen(3000)
